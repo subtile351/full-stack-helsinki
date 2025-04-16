45d76754e8f21730a95de5f9b34644dfcdbc11bc
Nikolai Andriakov <subtile58@gmail.com>
2025-04-16 11:46:27 +0300

    Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -48,12 +48,34 @@ const Statistics = (props) => {
     return (
       <>
         <h1>statistics</h1>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="total" value={total} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
+        <table>
+          <tbody>
+            <tr>
+              <td>good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>total</td>
+              <td>{total}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{average.toFixed(1)}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{positive.toFixed(1)}%</td>
+            </tr>
+          </tbody>
+        </table>
       </>
     )
 
