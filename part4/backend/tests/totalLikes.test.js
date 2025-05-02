1286d22c33826817c661ebc1732d4b463077f96f
Nikolai Andriakov <subtile58@gmail.com>
2025-05-02 21:12:23 +0300

    Added helper functions

---
+++ part4/backend/tests/totalLikes.test.js
@@ -0,0 +1,50 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('of a single item list is item\'s likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of many items list is the correct sum', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    assert.strictEqual(result, 10)
+  })
+})
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+  let sum = 0
+  if (blogs) {
+    blogs.forEach(blog => {
+      sum += typeof blog.likes === 'number' ? blog.likes : 0
+    })
+  }
+
+  return sum
 }
 
 module.exports = {
-    dummy
+  totalLikes
 }
