07b38cc58a5fb2901bf0b80c6b3bcb187d372ca6
Nikolai Andriakov <subtile58@gmail.com>
2025-05-02 20:41:29 +0300

    Refactored index.js and app.js

---
+++ part4/backend/app.js
@@ -0,0 +1,27 @@
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const express = require('express')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+logger.info('connecting to MongoDB')
+
+const mongoUrl = config.MONGODB_URI
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
