6de61fbe0aa7061f08735bb2f7a4a553603248dd
Nikolai Andriakov <subtile58@gmail.com>
2025-07-06 19:05:05 +0300

    Added GET request handler for users

--- part4/backend/controllers/users.js
+++ part4/backend/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const { request } = require('../app')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
@@ -17,6 +18,11 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
 })
 
 module.exports = usersRouter
