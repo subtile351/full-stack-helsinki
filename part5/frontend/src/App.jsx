d6c8cca845e2f379d0f49573f879fc512bb997e3
Nikolai Andriakov <subtile58@gmail.com>
2025-09-14 18:07:47 +0300

    Added login frontend and logout functionality

---
+++ part5/frontend/src/App.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON)
+      setUser(loggedUser)
+    }
+  }, [])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+  }
+
+  const loginForm = () => {
+    return(
+      <>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input
+                type="text"
+                value={username}
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input
+                type="password"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </>
+    )
+  }
+
+  const blogForm = () => {
+    return(
+      <>
+        <form onSubmit={addBlog}>
+          <input value={newBlog} onChange={handleBlogChange} />
+          <button type="submit">Save</button>
+        </form>
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.username} logged</p>
+          <button onClick={handleLogout}>Logout</button>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
